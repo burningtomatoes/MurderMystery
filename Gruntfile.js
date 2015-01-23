31718bfeb2bad0a23b69bd689d4c22323cf2eae7
Roy de Jong <roy@softwarepunt.nl>
2015-01-23 21:29:52 +0100

    Init: Set up basic source management. Configure grunt. Version IDE config.

---
+++ Gruntfile.js
@@ -0,0 +1,17 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> / www.burningtomato.com */\n'
+            },
+            build: {
+                src: ['src/3rdparty/inherit.js', 'src/settings.js', 'src/**/*.js'],
+                dest: 'build/<%= pkg.name %>.min.js'
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.registerTask('default', ['uglify']);
+};
---
+++ src/bootstrap.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    // This is a badass ASCII banner. It looks better in your console. :-)
+    console.log('  __  __               _           __  __           _');
+    console.log('|  \\/  |             | |         |  \\/  |         | |');
+    console.log('| \\  / |_   _ _ __ __| | ___ _ __| \\  / |_   _ ___| |_ ___ _ __ _   _');
+    console.log('| |\\/| | | | | \'__/ _` |/ _ \\ \'__| |\\/| | | | / __| __/ _ \\ \'__| | | |');
+    console.log('| |  | | |_| | | | (_| |  __/ |  | |  | | |_| \\__ \\ ||  __/ |  | |_| |');
+    console.log('|_|  |_|\\__,_|_|  \\__,_|\\___|_|  |_|  |_|\\__, |___/\\__\\___|_|   \\__, |');
+    console.log('                                          __/ |                  __/ |');
+    console.log('                                         |___/                  |___/ ');
+});
