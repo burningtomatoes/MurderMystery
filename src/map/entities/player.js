b6b15c5cd435461169a2ba59ec6db9bb60576a52
Roy de Jong <roy@softwarepunt.nl>
2015-01-24 23:50:38 +0100

    Player: Clean up, remove death/damage/attack code

--- src/map/entities/player.js
+++ src/map/entities/player.js
@@ -1,6 +1,4 @@
 var Player = Entity.extend({
-    damageFlash: 0,
-
     isTeleporting: false,
     teleportTo: null,
     teleportTimer: 0,
@@ -18,41 +16,8 @@ var Player = Entity.extend({
         this.spriteShadow = Gfx.load('shadow_body_generic');
     },
 
-    damage: function (changeValue) {
-        if (this.isTeleporting) {
-            return;
-        }
-
-        if (changeValue > 0 && !this.dead) {
-            this.damageFlash = 1;
-            Sfx.play('player_hurt.wav');
-        }
-
-        this._super(changeValue);
-
-        this.syncHealthUi();
-
-        Inventory.health = this.healthValue;
-    },
-
-    die: function() {
-        if (this.dead) {
-            return;
-        }
-
-        this._super();
-
-        this.damageFlash = Infinity;
-
-        Sfx.play('cinboom.wav');
-        Game.reset();
-    },
-
     update: function() {
-        var ourRect = this.getRect();
-
-        if (!this.isTeleporting && !this.dead && !this.isAttacking) {
-            // Movement input ///
+        if (!this.isTeleporting) {
             if (Keyboard.isKeyDown(KeyEvent.DOM_VK_LEFT) || Keyboard.isKeyDown(KeyEvent.DOM_VK_A)) {
                 this.velocityX = -this.movementSpeed;
                 this.direction = Direction.LEFT;
@@ -71,75 +36,11 @@ var Player = Entity.extend({
             } else {
                 this.velocityY = 0;
             }
-
-            // Attack input //
-            if (Keyboard.wasKeyPressed(KeyEvent.DOM_VK_SPACE)) {
-                this.attack();
-            }
-
-            // Red damage flash timer //
-            if (this.damageFlash > 0) {
-                this.damageFlash--;
-            }
-
-            // Entity collision (get hurt when you toch an enemy) //
-            if (this.hurtTimer <= 0 && this.touchPainTimer <= 0) {
-                var entities = Game.map.entities;
-                var entitiesLength = entities.length;
-
-                for (var i = 0; i < entitiesLength; i++) {
-                    var entity = entities[i];
-
-                    if (!entity.isNpc || !entity.causesDamage || entity.dead) {
-                        continue;
-                    }
-
-                    var theirRect = entity.getRect();
-
-                    if (Utils.rectIntersects(theirRect, ourRect)) {
-                        this.damage(1);
-                        this.touchPainTimer = 30;
-
-                        // Need to increment the pain timer because otherwise it would never render
-                        // (this code is above the entity update call, so it would be instantly brought to zero)
-                        this.hurtTimer++;
-
-                        break;
-                    }
-                }
-            }
-            else if (this.touchPainTimer > 0) {
-                this.touchPainTimer--;
-            }
-
-            // Pickups //
-            var entities = Game.map.entities;
-            var entitiesLength = entities.length;
-
-            for (var i = 0; i < entitiesLength; i++) {
-                var entity = entities[i];
-
-                if (!entity.isPickupable || !entity.instantPickup) {
-                    continue;
-                }
-
-                var theirRect = entity.getRect();
-
-                if (Utils.rectIntersects(theirRect, ourRect)) {
-                    entity.pickUp();
-                }
-            }
-        }
-
-        if (this.dead) {
-            this.damageFlash = Infinity;
         }
 
         this._super();
 
         if (this.isTeleporting) {
-            this.damageFlash = 0;
-
             if (this.teleportTimer > 0) {
                 this.teleportTimer--;
 
@@ -155,23 +56,5 @@ var Player = Entity.extend({
                 this.teleportTimer = 30;
             }
         }
-    },
-
-    sfxHurt: function () {
-        Sfx.play('player_hurt.wav');
-    },
-
-    sfxDeath: function () {
-        // TODO
-    },
-
-    draw: function(ctx) {
-        if (this.damageFlash > 0) {
-            ctx.rect(0, 0, Canvas.canvas.width, Canvas.canvas.height);
-            ctx.fillStyle = '#ff0000';
-            ctx.fill();
-        }
-
-        this._super(ctx);
     }
 });
