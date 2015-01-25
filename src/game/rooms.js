bcd285eebee107cec230f446d74921ad62f9091c
Roy de Jong <roy@softwarepunt.nl>
2015-01-25 22:28:32 +0100

    Rooms: Stub out room definitions & management

---
+++ src/game/rooms.js
@@ -0,0 +1,116 @@
+var Rooms = {
+    data: [],
+
+    push: function (room) {
+        this.data.push(room);
+    },
+
+    get: function (idx) {
+        return this.data[idx];
+    },
+
+    selectMurderRoom: function () {
+        var room = null;
+
+        do
+        {
+            var idx = chance.integer({
+                min: 0,
+                max: this.data.length - 1
+            });
+
+            room = this.data[idx];
+        }
+        while (!room.canMurder);
+
+        return room;
+    },
+
+    selectRandomRoom: function () {
+        var room = null;
+
+        var idx = chance.integer({
+            min: 0,
+            max: this.data.length - 1
+        });
+
+        return this.data[idx];
+    }
+};
+
+var Room = Class.extend({
+    /**
+     * The display name of this room, used in the UI and dialogue to reference it.
+     */
+    name: 'A Room',
+    /**
+     * Indicates whether a murder can happen in this room or not.
+     * This is almost always TRUE.
+     */
+    canMurder: true,
+    /**
+     * Indicates how many people can be in this room at the time of the murder.
+     * This is almost always 2, sometimes 1. Anything else would make the story weird.
+     */
+    maxOccupants: 2,
+    /**
+     * The name (ID) of the map file associated with this room.
+     */
+    mapId: null,
+
+    init: function (data) {
+        for (var property in data) {
+            var value = data[property];
+            this[property] = value;
+        }
+    }
+});
+
+Rooms.push(new Room({
+    name: 'Front Room',
+    canMurder: false,
+    maxOccupants: 0,
+    mapId: 'main_room'
+}));
+
+Rooms.push(new Room({
+    name: 'Bathroom',
+    canMurder: false,
+    maxOccupants: 1,
+    mapId: 'bath_room'
+}));
+
+Rooms.push(new Room({
+    name: 'Dining Room',
+    canMurder: true,
+    maxOccupants: 2,
+    mapId: 'dining_room'
+}));
+
+Rooms.push(new Room({
+    name: 'Master Bedroom',
+    canMurder: true,
+    maxOccupants: 2,
+    mapId: 'bed_room_1'
+}));
+
+Rooms.push(new Room({
+    name: 'Guest Bedroom',
+    canMurder: true,
+    maxOccupants: 2,
+    mapId: 'bed_room_2'
+}));
+
+Rooms.push(new Room({
+    name: 'Study',
+    canMurder: true,
+    maxOccupants: 2,
+    mapId: 'office_room'
+}));
+
+Rooms.push(new Room({
+    name: 'Rec Room',
+    canMurder: true,
+    maxOccupants: 2,
+    mapId: 'billiard_room'
+}));
