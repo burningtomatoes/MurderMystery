381ab8440430d1a7f170be26ced235c3816cba55
Roy de Jong <roy@softwarepunt.nl>
2015-01-27 23:36:10 +0100

    Guests: Randomly wander about the living area.

--- src/map/entities/guest.js
+++ src/map/entities/guest.js
@@ -2,8 +2,6 @@ var Guest = Entity.extend({
     isNpc: true,
     isGuest: true,
 
-    restlessTimer: 0,
-
     firstName: 'John',
     lastName: 'Doe',
     gender: Gender.MALE,
@@ -28,6 +26,8 @@ var Guest = Entity.extend({
 
         this.width = 40;
         this.height = 26;
+
+        this.movementSpeed = 1;
 
         this.spriteHead = Gfx.load('head_' + head);
         this.spriteBody = Gfx.load('body_' + body);
@@ -74,7 +74,89 @@ var Guest = Entity.extend({
         return this.getNamePrefix() + ' ' + this.firstName + ' ' + this.lastName;
     },
 
+    restlessTimer: 0,
+    tiredTimer: 0,
+    currentAction: null,
+
+    doSomething: function () {
+        this.restlessTimer = chance.integer({
+            min: 60,
+            max: 600
+        });
+
+        this.tiredTimer = -chance.integer({
+            min: 60,
+            max: 600
+        });
+
+        var direction = chance.integer({
+            min: 0,
+            max: 4
+        });
+
+        switch (direction) {
+            case Direction.UP:
+                this.direction = Direction.UP;
+                this.velocityX = 0;
+                this.velocityY = -this.movementSpeed;
+                break;
+
+            case Direction.LEFT:
+                this.direction = Direction.LEFT;
+                this.velocityX = -this.movementSpeed;
+                this.velocityY = 0;
+                break;
+
+            case Direction.RIGHT:
+                this.direction = Direction.RIGHT;
+                this.velocityX = +this.movementSpeed;
+                this.velocityY = 0;
+                break;
+
+            case Direction.DOWN:
+                this.direction = Direction.DOWN;
+                this.velocityX = 0;
+                this.velocityY = +this.movementSpeed;
+                break;
+        }
+    },
+
+    isWalking: function () {
+        return this.velocityX != 0 || this.velocityY != 0;
+    },
+
+    isBored: function () {
+        if (this.restlessTimer > 0) {
+            this.restlessTimer--;
+        }
+
+        return this.restlessTimer == 0;
+    },
+
+    isTired: function () {
+        if (this.tiredTimer < 0) {
+            this.tiredTimer++;
+        }
+
+        return this.tiredTimer >= 0;
+    },
+
+    stopWalking: function () {
+        this.velocityX = 0;
+        this.velocityY = 0;
+    },
+
     update: function () {
         this._super();
+
+        if (this.isBored()) {
+            this.doSomething();
+        }
+
+        if (this.isWalking()) {
+            if (this.isTired()) {
+                this.stopWalking();
+            }
+        }
     }
 });
